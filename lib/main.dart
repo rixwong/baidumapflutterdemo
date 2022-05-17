b7c8309aca8e0d06a84b266fc47cdc1039cb859b
rixwong <1423551936@qq.com>
2022-05-17 23:33:56 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+import 'package:flutter_bmfmap_example/CustomWidgets/map_appbar.dart';
+import 'package:flutter_baidu_mapapi_base/flutter_baidu_mapapi_base.dart'
+    show BMFMapSDK, BMF_COORD_TYPE;
+import 'flutter_map_demo.dart';
+
+Future<void> main() async {
+  runApp(new MyApp());
+
+  /// 设置用户是否同意SDK隐私协议
+  /// since 3.1.0 开发者必须设置
+  BMFMapSDK.setAgreePrivacy(true);
+
+  // 百度地图sdk初始化鉴权
+  if (Platform.isIOS) {
+    BMFMapSDK.setApiKeyAndCoordType(
+        'R0kfqkT4dRLjyMQgbKhvCz0WN8yn6nvR', BMF_COORD_TYPE.BD09LL);
+  } else if (Platform.isAndroid) {
+    // Android 目前不支持接口设置Apikey,
+    // 请在主工程的Manifest文件里设置，详细配置方法请参考官网(https://lbsyun.baidu.com/)demo
+    BMFMapSDK.setCoordType(BMF_COORD_TYPE.BD09LL);
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: BMFAppBar(
+            title: '百度地图flutter插件Demo',
+            isBack: false,
+          ),
+          body: FlutterBMFMapDemo()
+      )
+    );
+  }
+}
+
+class SampleApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key? key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  // Default placeholder text.
+  String textToShow = 'I Like Flutter';
+
+  void _updateText() {
+    setState(() {
+      // Update the text.
+      textToShow = 'Flutter is Awesome!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sample App'),
+      ),
+      body: Center(child: Text(textToShow)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateText,
+        tooltip: 'Update Text',
+        child: Icon(Icons.update),
+      ),
+    );
+  }
+}
