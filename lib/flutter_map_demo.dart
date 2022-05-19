9635bf953e9d2afcef0ede349886822191bdf915
rixwong <1423551936@qq.com>
2022-05-19 14:26:56 +0800

    此时有两个功能：地图SDK和定位SDK

--- lib/flutter_map_demo.dart
+++ lib/flutter_map_demo.dart
@@ -7,6 +7,11 @@ import 'package:flutter_bmfmap_example/CustomWidgets/function_item.widget.dart';
 import 'package:flutter_bmfmap_example/DemoPages/Utils/flutter_maputils_demo.dart';
 import 'DemoPages/SearchRoute/show_search_route_page.dart';
 import 'DemoPages/search/common/show_search_page.dart';
+import 'location/circleGeofencePage.dart';
+import 'location/headingLocationPage.dart';
+import 'location/polygonGeofencePage.dart';
+import 'location/seriesLocationPage.dart';
+import 'location/singleLocationPage.dart';
 import 'my_test_widget.dart';
 
 class FlutterBMFMapDemo extends StatelessWidget {
@@ -21,40 +26,67 @@ class FlutterBMFMapDemo extends StatelessWidget {
           sublabel: '基础地图、个性化地图、TextureMapView、离线地图、室内地图以及多地图创建',
           target: ShowCreateMapPage(),
         ),
-        // FunctionItem(
-        //   label: '图层展示',
-        //   sublabel: '卫星图、交通流量图、百度城市热力图、3D地图及定位图层的展示',
-        //   target: ShowLayerMapPage(),
-        // ),
-        // FunctionItem(
-        //   label: '在地图上绘制',
-        //   sublabel: '介绍自定义绘制点、线、多边形、圆等几何图形和文字',
-        //   target: MapDrawPage(),
-        // ),
-        // FunctionItem(
-        //   label: '与地图交互',
-        //   sublabel: '介绍地图基本控制方法，事件响应、手势控制以及UI控件的显示与隐藏',
-        //   target: ShowInteractPage(),
-        // ),
-        // FunctionItem(
-        //   label: '检索地图数据',
-        //   sublabel: '检索和展示地图上的数据',
-        //   target: ShowSearchPage(),
-        // ),
-        // FunctionItem(
-        //   label: '路线规划',
-        //   sublabel: '各种场景下的路线规划',
-        //   target: ShowSearchRoutePage(),
-        // ),
-        // FunctionItem(
-        //   label: '实用工具',
-        //   sublabel: '调起百度地图等功能',
-        //   target: FlutterBMFUtilsDemo(),
-        // ),
+        FunctionItem(
+          label: '图层展示',
+          sublabel: '卫星图、交通流量图、百度城市热力图、3D地图及定位图层的展示',
+          target: ShowLayerMapPage(),
+        ),
+        FunctionItem(
+          label: '在地图上绘制',
+          sublabel: '介绍自定义绘制点、线、多边形、圆等几何图形和文字',
+          target: MapDrawPage(),
+        ),
+        FunctionItem(
+          label: '与地图交互',
+          sublabel: '介绍地图基本控制方法，事件响应、手势控制以及UI控件的显示与隐藏',
+          target: ShowInteractPage(),
+        ),
+        FunctionItem(
+          label: '检索地图数据',
+          sublabel: '检索和展示地图上的数据',
+          target: ShowSearchPage(),
+        ),
+        FunctionItem(
+          label: '路线规划',
+          sublabel: '各种场景下的路线规划',
+          target: ShowSearchRoutePage(),
+        ),
+        FunctionItem(
+          label: '实用工具',
+          sublabel: '调起百度地图等功能',
+          target: FlutterBMFUtilsDemo(),
+        ),
         FunctionItem(
           label: 'testMyMap',
           sublabel: '调起百度地图',
           target: TestBaiduMapPage(),
+        ),
+        FunctionItem(
+          label: '单次定位',
+          sublabel: 'singleLocationPage',
+          target: SingleLocationPage(),
+        ),
+
+        ///需要停止定位
+        FunctionItem(
+          label: '连续定位',
+          sublabel: 'seriesLocationPage',
+          target: SeriesLocationPage(),
+        ),
+        FunctionItem(
+          label: '圆形地理围栏',
+          sublabel: 'circleGeofencePage',
+          target: CircleGeofencePage(),
+        ),
+        FunctionItem(
+          label: '多边形地理围栏',
+          sublabel: 'polygonGeofencePage',
+          target: PolygonGeofencePage(),
+        ),
+        FunctionItem(
+          label: '监听设备朝向',
+          sublabel: 'headingLocationPage',
+          target: HeadingLocationPage(),
         ),
       ]),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
+import 'package:flutter_bmflocation/flutter_bmflocation.dart';
 import 'package:flutter_bmfmap_example/CustomWidgets/map_appbar.dart';
 import 'package:flutter_baidu_mapapi_base/flutter_baidu_mapapi_base.dart'
     show BMFMapSDK, BMF_COORD_TYPE;
+import 'package:permission_handler/permission_handler.dart';
 import 'flutter_map_demo.dart';
 
 Future<void> main() async {
   runApp(new MyApp());
+  /// 动态申请定位权限
+  requestPermission();
 
+  LocationFlutterPlugin myLocPlugin = LocationFlutterPlugin();
+  // NOTE: 各个SDK的隐私合规接口相互独立，若您同时集成地图SDK和定位SDK，需要分别调用隐私合规接口：地图初始化前请设置！！！
+  myLocPlugin.setAgreePrivacy(true);
   /// 设置用户是否同意SDK隐私协议
   /// since 3.1.0 开发者必须设置
   BMFMapSDK.setAgreePrivacy(true);
+  // NOTE: 各个SDK的隐私合规接口相互独立，若您同时集成地图SDK和定位SDK，需要分别调用隐私合规接口：地图初始化前请设置！！！
 
   // 百度地图sdk初始化鉴权
   if (Platform.isIOS) {
@@ -20,6 +28,37 @@ Future<void> main() async {
     // Android 目前不支持接口设置Apikey,
     // 请在主工程的Manifest文件里设置，详细配置方法请参考官网(https://lbsyun.baidu.com/)demo
     BMFMapSDK.setCoordType(BMF_COORD_TYPE.BD09LL);
+  }
+  /// iOS端鉴权结果 （指定位SDK的！！）
+  myLocPlugin.getApiKeyCallback(callback: (String result) {
+  String str = result;
+  print('鉴权结果：' + str);
+  });
+}
+// 动态申请定位权限
+void requestPermission() async {
+  // 申请权限
+  bool hasLocationPermission = await requestLocationPermission();
+  if (hasLocationPermission) {
+    // 权限申请通过
+  } else {}
+}
+/// 申请定位权限
+/// 授予定位权限返回true， 否则返回false
+Future<bool> requestLocationPermission() async {
+  //获取当前的权限
+  var status = await Permission.location.status;
+  if (status == PermissionStatus.granted) {
+    //已经授权
+    return true;
+  } else {
+    //未授权则发起一次申请
+    status = await Permission.location.request();
+    if (status == PermissionStatus.granted) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
@@ -48,50 +87,50 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class SampleApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sample App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SampleAppPage(),
-    );
-  }
-}
-
-class SampleAppPage extends StatefulWidget {
-  SampleAppPage({Key? key}) : super(key: key);
-
-  @override
-  _SampleAppPageState createState() => _SampleAppPageState();
-}
-
-class _SampleAppPageState extends State<SampleAppPage> {
-  // Default placeholder text.
-  String textToShow = 'I Like Flutter';
-
-  void _updateText() {
-    setState(() {
-      // Update the text.
-      textToShow = 'Flutter is Awesome!';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sample App'),
-      ),
-      body: Center(child: Text(textToShow)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updateText,
-        tooltip: 'Update Text',
-        child: Icon(Icons.update),
-      ),
-    );
-  }
-}
+// class SampleApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Sample App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SampleAppPage(),
+//     );
+//   }
+// }
+//
+// class SampleAppPage extends StatefulWidget {
+//   SampleAppPage({Key? key}) : super(key: key);
+//
+//   @override
+//   _SampleAppPageState createState() => _SampleAppPageState();
+// }
+//
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   // Default placeholder text.
+//   String textToShow = 'I Like Flutter';
+//
+//   void _updateText() {
+//     setState(() {
+//       // Update the text.
+//       textToShow = 'Flutter is Awesome!';
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sample App'),
+//       ),
+//       body: Center(child: Text(textToShow)),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _updateText,
+//         tooltip: 'Update Text',
+//         child: Icon(Icons.update),
+//       ),
+//     );
+//   }
+// }
